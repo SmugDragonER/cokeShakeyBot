7c20ebca289a834e3f1de713fa9399e4fdaaeab3
William <william.taglieber@tha.de>
2025-05-12 10:30:01 +0200

    edited help message

--- commands/help.py
+++ commands/help.py
@@ -6,7 +6,7 @@ async def handle_help(send_message_function, channel_id: int) -> None:
                     f"**!register [your text goes here]**\n"
                     f"bot will copy your message and add reactions to it\n\n"
                     f"**!update**\n"
-                    f"CURRENTLY NOT WORKING, bot will update final signup messages with the highest MMR account of every player\n\n"
+                    f"NOT TESTED, Bot will update final signup messages with the highest MMR account of every player\n\n"
                     f"**!teamrank**\n"
                     f"bot will send the rank of every teammembers account, sorted from highest to lowest\n\n")
     await send_message_function(channel_id, help_message)
